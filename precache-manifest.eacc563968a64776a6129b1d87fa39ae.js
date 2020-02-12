e13dae2aa5a1058d2a8db2620e436b81ce1e5d41
tg-developer <tg-developer@gmail.com>
2020-02-12 13:06:42 -0300

    Updates

---
+++ precache-manifest.eacc563968a64776a6129b1d87fa39ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7adde43d63a94025b8f391c76722005",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d224d146f6f27c42a0e9",
+    "url": "/react-gh-pages/static/css/main.f97dc3a0.chunk.css"
+  },
+  {
+    "revision": "0e5c8dda3eebe28c3290",
+    "url": "/react-gh-pages/static/js/2.b3bf0c97.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/react-gh-pages/static/js/2.b3bf0c97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d224d146f6f27c42a0e9",
+    "url": "/react-gh-pages/static/js/main.e44a02a1.chunk.js"
+  },
+  {
+    "revision": "05bc9796946221346c65",
+    "url": "/react-gh-pages/static/js/runtime-main.4411ca3d.js"
+  }
+]);
